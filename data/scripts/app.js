2123f90dfab368636e217798227f320ce501dbab
nkanand <nkanand@gmail.com>
2015-08-11 08:03:02 -0700

    Update app.js

--- data/scripts/app.js
+++ data/scripts/app.js
@@ -5,4 +5,12 @@
     $scope.logo = './icon-32.png';
 }
 var Mymodule = angular.module('MyAddon', [])
+    .config( [
+        '$compileProvider',
+        function( $compileProvider )
+        {
+            $compileProvider.imgSrcSanitizationWhitelist(/^\s*(filesystem:resource|resource):/);
+            // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+        }
+    ])
     .controller('MainCtrl', MainController)
